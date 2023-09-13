bbfa9610a0c445b676046896fa5349c064cb32cb
pyratu <paul_pyratu@yahoo.com>
2023-09-13 12:00:59 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# CPP Rock Paper Scissors
- 
+## CLI Rock Paper Scissors Showdown 🗿 📄 ✂️
+Welcome to my Rock Paper Scissors project, where I've honed my coding skills through this challenge.
+
+Feel free to customize it further to better represent your journey in coding.
+
+## How to Play
+Clone or download this repository to your local machine.
+Compile the program using a C++ compiler.
+Run the compiled executable.
+Game Rules
+Make your move by choosing one of the following options:
+
+1) Rock ✊
+2) Paper 📄
+3) Scissors ✂️ 
+4) To EXIT press 4
+
+The AI opponent will randomly select its move.
+
+## Determine the winner based on the classic rules:
+
+✊ Rock beats ✂️ Scissors
+📄 Paper beats ✊ Rock
+✂️ Scissors beats 📄 Paper
+
+Keep an eye on the win streaks for both you and the AI.
+When you're ready to exit, simply choose option 4.
+
+## Features
+Persistent Stats: Your victories and the AI's triumphs are tracked and saved in a file (stats.txt) to ensure the competition continues across sessions.
---
+++ rps.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int winStreakUser = 0;
+int winStreakAi = 0;
+
+void menu();
+string ChooseWinner(int userChoice, int aiChoice);
+void saveStatsToFile();
+void loadStatsFromFile();
+
+int main()
+{
+    loadStatsFromFile(); // Load win streaks from a file at the beginning
+    int userChoice = 0;
+
+    do
+    {
+        menu();
+        cin >> userChoice;
+
+        if (userChoice == 4)
+        {
+            cout << "Game Over." << endl;
+            cout << "Ai won " << winStreakAi << " times." << endl;
+            cout << "Player won " << winStreakUser << " times." << endl;
+
+            saveStatsToFile(); // Save win streaks to a file before exiting
+            break;
+        }
+
+        int aiChoice = rand() % 3 + 1;
+        string winner = ChooseWinner(userChoice, aiChoice);
+        cout << "Round ended. " << winner << endl
+             << endl;
+    } while (userChoice != 4);
+}
+
+void menu()
+{
+    cout << "Player wins: " << winStreakUser << endl;
+    cout << "Ai wins: " << winStreakAi << endl;
+    cout << "Choose " << endl;
+    cout << "1 Rock" << endl;
+    cout << "2 Paper" << endl;
+    cout << "3 Scissors" << endl;
+    cout << "4 To EXIT" << endl;
+}
+
+string ChooseWinner(int userChoice, int aiChoice)
+{
+    string winner = "Draw";
+
+    if (userChoice == 1 && aiChoice == 2)
+    {
+        winStreakAi++;
+        return "Ai wins";
+    }
+
+    if (userChoice == 1 && aiChoice == 3)
+    {
+        winStreakUser++;
+        return "Player wins";
+    }
+
+    if (userChoice == 2 && aiChoice == 1)
+    {
+        winStreakUser++;
+        return "Player wins";
+    }
+
+    if (userChoice == 2 && aiChoice == 3)
+    {
+        winStreakAi++;
+        return "Ai wins";
+    }
+
+    if (userChoice == 3 && aiChoice == 1)
+    {
+        winStreakAi++;
+        return "Ai wins";
+    }
+
+    if (userChoice == 3 && aiChoice == 2)
+    {
+        winStreakUser++;
+        return "Player wins";
+    }
+
+    if (userChoice != 1 && userChoice != 2 && userChoice != 3)
+        menu();
+
+    return "Draw";
+}
+
+void saveStatsToFile()
+{
+    ofstream outputFile("stats.txt");
+    if (outputFile.is_open())
+    {
+        outputFile << winStreakUser << " " << winStreakAi;
+        outputFile.close();
+        cout << "Stats saved successfully." << endl;
+    }
+    else
+    {
+        cerr << "Error opening file for writing." << endl;
+    }
+}
+
+void loadStatsFromFile()
+{
+    ifstream inputFile("stats.txt");
+    if (inputFile.is_open())
+    {
+        if (inputFile >> winStreakUser >> winStreakAi)
+        {
+            cout << "Stats loaded successfully." << endl;
+        }
+        else
+        {
+            cerr << "Error reading stats from file." << endl;
+        }
+        inputFile.close();
+    }
+    else
+    {
+        cerr << "Error opening file for reading. Starting with fresh stats." << endl;
+    }
+}
